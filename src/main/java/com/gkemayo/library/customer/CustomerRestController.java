9df4c380cbde7be218bfb4b07968f5fb69473399
Kg <misterkgainfo@yahoo.fr>
2019-07-06 14:48:38 +0200

    First commit of the project

---
+++ src/main/java/com/gkemayo/library/customer/CustomerRestController.java
@@ -0,0 +1,211 @@
+package com.gkemayo.library.customer;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@RestController
+@RequestMapping("/rest/customer/api")
+@Api(value = "Customer Rest Controller: contains all operations for managing customers")
+public class CustomerRestController {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(CustomerRestController.class);
+
+	@Autowired
+	private CustomerServiceImpl customerService;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
+
+	@PostMapping("/addCustomer")
+	@ApiOperation(value = "Add a new Customer in the Library", response = CustomerDTO.class)
+	@ApiResponses(value = { @ApiResponse(code = 409, message = "Conflict: the customer already exist"),
+			@ApiResponse(code = 201, message = "Created: the customer is successfully inserted"),
+			@ApiResponse(code = 304, message = "Not Modified: the customer is unsuccessfully inserted") })
+	public ResponseEntity<CustomerDTO> createNewCustomer(@RequestBody CustomerDTO customerDTORequest) {
+		//, UriComponentsBuilder uriComponentBuilder
+		Customer existingCustomer = customerService.findCustomerByEmail(customerDTORequest.getEmail());
+		if (existingCustomer != null) {
+			return new ResponseEntity<CustomerDTO>(HttpStatus.CONFLICT);
+		}
+		Customer customerRequest = mapCustomerDTOToCustomer(customerDTORequest);
+		customerRequest.setCreationDate(LocalDate.now());
+		Customer customerResponse = customerService.saveCustomer(customerRequest);
+		if (customerResponse != null) {
+			CustomerDTO customerDTO = mapCustomerToCustomerDTO(customerResponse);
+			return new ResponseEntity<CustomerDTO>(customerDTO, HttpStatus.CREATED);
+		}
+		return new ResponseEntity<CustomerDTO>(HttpStatus.NOT_MODIFIED);
+
+	}
+
+	@PutMapping("/updateCustomer")
+	@ApiOperation(value = "Update/Modify an existing customer in the Library", response = CustomerDTO.class)
+	@ApiResponses(value = { @ApiResponse(code = 404, message = "Not Found : the customer does not exist"),
+			@ApiResponse(code = 200, message = "Ok: the customer is successfully updated"),
+			@ApiResponse(code = 304, message = "Not Modified: the customer is unsuccessfully updated") })
+	public ResponseEntity<CustomerDTO> updateCustomer(@RequestBody CustomerDTO customerDTORequest) {
+		//, UriComponentsBuilder uriComponentBuilder
+		if (!customerService.checkIfIdexists(customerDTORequest.getId())) {
+			return new ResponseEntity<CustomerDTO>(HttpStatus.NOT_FOUND);
+		}
+		Customer customerRequest = mapCustomerDTOToCustomer(customerDTORequest);
+		Customer customerResponse = customerService.updateCustomer(customerRequest);
+		if (customerResponse != null) {
+			CustomerDTO customerDTO = mapCustomerToCustomerDTO(customerResponse);
+			return new ResponseEntity<CustomerDTO>(customerDTO, HttpStatus.OK);
+		}
+		return new ResponseEntity<CustomerDTO>(HttpStatus.NOT_MODIFIED);
+	}
+
+	@DeleteMapping("/deleteCustomer/{customerId}")
+	@ApiOperation(value = "Delete a customer in the Library, if the customer does not exist, nothing is done", response = String.class)
+	@ApiResponse(code = 204, message = "No Content: customer sucessfully deleted")
+	public ResponseEntity<String> deleteCustomer(@PathVariable Integer customerId) {
+		customerService.deleteCustomer(customerId);
+		return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+	}
+
+	@GetMapping("/paginatedSearch")
+	@ApiOperation(value="List customers of the Library in a paginated way", response = List.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Ok: successfully listed"),
+			@ApiResponse(code = 204, message = "No Content: no result founded"),
+	})
+	public ResponseEntity<List<CustomerDTO>> searchCustomers(@RequestParam("beginPage") int beginPage,
+			@RequestParam("endPage") int endPage) {
+		//, UriComponentsBuilder uriComponentBuilder
+		Page<Customer> customers = customerService.getPaginatedCustomersList(beginPage, endPage);
+		if (customers != null) {
+			List<CustomerDTO> customerDTOs = customers.stream().map(customer -> {
+				return mapCustomerToCustomerDTO(customer);
+			}).collect(Collectors.toList());
+			return new ResponseEntity<List<CustomerDTO>>(customerDTOs, HttpStatus.OK);
+		}
+		return new ResponseEntity<List<CustomerDTO>>(HttpStatus.NO_CONTENT);
+	}
+
+	@GetMapping("/searchByEmail")
+	@ApiOperation(value="Search a customer in the Library by its email", response = CustomerDTO.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Ok: successfull research"),
+			@ApiResponse(code = 204, message = "No Content: no result founded"),
+	})
+	public ResponseEntity<CustomerDTO> searchCustomerByEmail(@RequestParam("email") String email) {
+		//, UriComponentsBuilder uriComponentBuilder
+		Customer customer = customerService.findCustomerByEmail(email);
+		if (customer != null) {
+			CustomerDTO customerDTO = mapCustomerToCustomerDTO(customer);
+			return new ResponseEntity<CustomerDTO>(customerDTO, HttpStatus.OK);
+		}
+		return new ResponseEntity<CustomerDTO>(HttpStatus.NO_CONTENT);
+	}
+	
+	@GetMapping("/searchByLastName")
+	@ApiOperation(value="Search a customer in the Library by its Last name", response = List.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Ok: successfull research"),
+			@ApiResponse(code = 204, message = "No Content: no result founded"),
+	})
+	public ResponseEntity<List<CustomerDTO>> searchBookByLastName(@RequestParam("lastName") String lastName) {
+		//,	UriComponentsBuilder uriComponentBuilder
+		List<Customer> customers = customerService.findCustomerByLastName(lastName);
+		if (customers != null && !CollectionUtils.isEmpty(customers)) {
+			List<CustomerDTO> customerDTOs = customers.stream().map(customer -> {
+				return mapCustomerToCustomerDTO(customer);
+			}).collect(Collectors.toList());
+			return new ResponseEntity<List<CustomerDTO>>(customerDTOs, HttpStatus.OK);
+		}
+		return new ResponseEntity<List<CustomerDTO>>(HttpStatus.NO_CONTENT);
+	}
+	
+	@PutMapping("/sendEmailToCustomer")
+	@ApiOperation(value="Send an email to customer of the Library", response = String.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Ok: Email successfully sent"),
+			@ApiResponse(code = 404, message = "Not Found: no customer found, or wrong email"),
+			@ApiResponse(code = 403, message = "Forbidden: Email cannot be sent")
+	})
+	public ResponseEntity<Boolean> sendMailToCustomer(@RequestBody MailDTO loanMailDto, UriComponentsBuilder uriComponentBuilder) {
+
+		Customer customer = customerService.findCustomerById(loanMailDto.getCustomerId());
+		if (customer == null) {
+			String errorMessage = "The selected Customer for sending email is not found in the database";
+			LOGGER.info(errorMessage);
+			return new ResponseEntity<Boolean>(false, HttpStatus.NOT_FOUND);
+		} else if (customer != null && StringUtils.isEmpty(customer.getEmail())) {
+			String errorMessage = "No existing email for the selected Customer for sending email to";
+			LOGGER.info(errorMessage);
+			return new ResponseEntity<Boolean>(false, HttpStatus.NOT_FOUND);
+		}
+
+		SimpleMailMessage mail = new SimpleMailMessage();
+		mail.setFrom(loanMailDto.MAIL_FROM);
+		mail.setTo(customer.getEmail());
+		mail.setSentDate(new Date());
+		mail.setSubject(loanMailDto.getEmailSubject());
+		mail.setText(loanMailDto.getEmailContent());
+
+		try {
+			javaMailSender.send(mail);
+		} catch (MailException e) {
+			return new ResponseEntity<Boolean>(false, HttpStatus.FORBIDDEN);
+		}
+
+		return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+	}
+
+	/**
+	 * Transforme un Customer en CustomerDTO
+	 * 
+	 * @param customer
+	 * @return
+	 */
+	private CustomerDTO mapCustomerToCustomerDTO(Customer customer) {
+		ModelMapper mapper = new ModelMapper();
+		CustomerDTO customerDTO = mapper.map(customer, CustomerDTO.class);
+		return customerDTO;
+	}
+
+	/**
+	 * Transforme un CustomerDTO en Customer
+	 * 
+	 * @param customerDTO
+	 * @return
+	 */
+	private Customer mapCustomerDTOToCustomer(CustomerDTO customerDTO) {
+		ModelMapper mapper = new ModelMapper();
+		Customer customer = mapper.map(customerDTO, Customer.class);
+		return customer;
+	}
+
+}
---
+++ src/main/webapp/main.js
@@ -0,0 +1,288 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"],{
+
+/***/ "./src/$$_lazy_route_resource lazy recursive":
+/*!**********************************************************!*\
+  !*** ./src/$$_lazy_route_resource lazy namespace object ***!
+  \**********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncaught exception popping up in devtools
+	return Promise.resolve().then(function() {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
+
+/***/ }),
+
+/***/ "./src/app/app-routing.module.ts":
+/*!***************************************!*\
+  !*** ./src/app/app-routing.module.ts ***!
+  \***************************************/
+/*! exports provided: AppRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppRoutingModule", function() { return AppRoutingModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var src_app_book_add_book_add_book_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! src/app/book/add-book/add-book.component */ "./src/app/book/add-book/add-book.component.ts");
+
+
+
+
+var routes = [
+    {
+        path: 'add-book', component: src_app_book_add_book_add_book_component__WEBPACK_IMPORTED_MODULE_3__["AddBookComponent"]
+    }
+];
+var AppRoutingModule = /** @class */ (function () {
+    function AppRoutingModule() {
+    }
+    AppRoutingModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forRoot(routes)],
+            exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]]
+        })
+    ], AppRoutingModule);
+    return AppRoutingModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.component.css":
+/*!***********************************!*\
+  !*** ./src/app/app.component.css ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2FwcC5jb21wb25lbnQuY3NzIn0= */"
+
+/***/ }),
+
+/***/ "./src/app/app.component.html":
+/*!************************************!*\
+  !*** ./src/app/app.component.html ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<!--The content below is only a placeholder and can be replaced.-->\n<div style=\"text-align:center\">\n  <h1>\n    Welcome to {{ title }}!\n  </h1>\n  <img width=\"300\" alt=\"Angular Logo\" src=\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==\">\n</div>\n<h2>Here are some links to help you start: </h2>\n<ul>\n  <li>\n    <h2><a target=\"_blank\" rel=\"noopener\" href=\"https://angular.io/tutorial\">Tour of Heroes</a></h2>\n  </li>\n  <li>\n    <h2><a target=\"_blank\" rel=\"noopener\" href=\"https://angular.io/cli\">CLI Documentation</a></h2>\n  </li>\n  <li>\n    <h2><a target=\"_blank\" rel=\"noopener\" href=\"https://blog.angular.io/\">Angular blog</a></h2>\n  </li>\n</ul>\n\n<router-outlet></router-outlet>\n"
+
+/***/ }),
+
+/***/ "./src/app/app.component.ts":
+/*!**********************************!*\
+  !*** ./src/app/app.component.ts ***!
+  \**********************************/
+/*! exports provided: AppComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppComponent", function() { return AppComponent; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.title = 'library-ui';
+    }
+    AppComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-root',
+            template: __webpack_require__(/*! ./app.component.html */ "./src/app/app.component.html"),
+            styles: [__webpack_require__(/*! ./app.component.css */ "./src/app/app.component.css")]
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.module.ts":
+/*!*******************************!*\
+  !*** ./src/app/app.module.ts ***!
+  \*******************************/
+/*! exports provided: AppModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppModule", function() { return AppModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/platform-browser */ "./node_modules/@angular/platform-browser/fesm5/platform-browser.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _app_routing_module__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./app-routing.module */ "./src/app/app-routing.module.ts");
+/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
+/* harmony import */ var _book_add_book_add_book_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./book/add-book/add-book.component */ "./src/app/book/add-book/add-book.component.ts");
+
+
+
+
+
+
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_2__["NgModule"])({
+            declarations: [
+                _app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"],
+                _book_add_book_add_book_component__WEBPACK_IMPORTED_MODULE_5__["AddBookComponent"]
+            ],
+            imports: [
+                _angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__["BrowserModule"],
+                _app_routing_module__WEBPACK_IMPORTED_MODULE_3__["AppRoutingModule"]
+            ],
+            providers: [],
+            bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_4__["AppComponent"]]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/book/add-book/add-book.component.css":
+/*!******************************************************!*\
+  !*** ./src/app/book/add-book/add-book.component.css ***!
+  \******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2Jvb2svYWRkLWJvb2svYWRkLWJvb2suY29tcG9uZW50LmNzcyJ9 */"
+
+/***/ }),
+
+/***/ "./src/app/book/add-book/add-book.component.html":
+/*!*******************************************************!*\
+  !*** ./src/app/book/add-book/add-book.component.html ***!
+  \*******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<div>\n\t<form action=\"\">\n\t\t<label>Isbn:</label><input type=\"text\">\n\t\t<label>Title:</label><input type=\"text\">\n\t\t<label>Author:</label><input type=\"text\">\n\t\t<label>Total examplaries:</label><input type=\"text\">\n\t\t<label>Category:</label><select><option></select>\n\t</form>\n</div>\n"
+
+/***/ }),
+
+/***/ "./src/app/book/add-book/add-book.component.ts":
+/*!*****************************************************!*\
+  !*** ./src/app/book/add-book/add-book.component.ts ***!
+  \*****************************************************/
+/*! exports provided: AddBookComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AddBookComponent", function() { return AddBookComponent; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+var AddBookComponent = /** @class */ (function () {
+    function AddBookComponent() {
+    }
+    AddBookComponent.prototype.ngOnInit = function () {
+    };
+    AddBookComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-add-book',
+            template: __webpack_require__(/*! ./add-book.component.html */ "./src/app/book/add-book/add-book.component.html"),
+            styles: [__webpack_require__(/*! ./add-book.component.css */ "./src/app/book/add-book/add-book.component.css")]
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [])
+    ], AddBookComponent);
+    return AddBookComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/environments/environment.ts":
+/*!*****************************************!*\
+  !*** ./src/environments/environment.ts ***!
+  \*****************************************/
+/*! exports provided: environment */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+var environment = {
+    production: false
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+/***/ }),
+
+/***/ "./src/main.ts":
+/*!*********************!*\
+  !*** ./src/main.ts ***!
+  \*********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/platform-browser-dynamic */ "./node_modules/@angular/platform-browser-dynamic/fesm5/platform-browser-dynamic.js");
+/* harmony import */ var _app_app_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/app.module */ "./src/app/app.module.ts");
+/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./environments/environment */ "./src/environments/environment.ts");
+
+
+
+
+if (_environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].production) {
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["enableProdMode"])();
+}
+Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformBrowserDynamic"])().bootstrapModule(_app_app_module__WEBPACK_IMPORTED_MODULE_2__["AppModule"])
+    .catch(function (err) { return console.error(err); });
+
+
+/***/ }),
+
+/***/ 0:
+/*!***************************!*\
+  !*** multi ./src/main.ts ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! E:\Dev-since04022019\Sources\library\src\main\website\library-ui\src\main.ts */"./src/main.ts");
+
+
+/***/ })
+
+},[[0,"runtime","vendor"]]]);
+//# sourceMappingURL=main.js.map
